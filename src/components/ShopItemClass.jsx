6b58fed82c07a8c1e3bbdccc74cbd9115f78ea8f
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2019-08-06 12:28:38 +0300

    task completed

---
+++ src/components/ShopItemClass.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Item } from '../models/Item';
+
+class ShopItemClass extends Component {
+  static propTypes = {
+    item: PropTypes.instanceOf(Item).isRequired,
+  };
+
+  render() {
+    const { item } = this.props;
+    return (
+      <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.name}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">{item.descriptionFull}</div>
+        <div className="highlight-window  mobile">
+          <div className="highlight-overlay" />
+        </div>
+        <div className="divider" />
+        <div className="purchase-info">
+          <div className="price">
+            {item.currency}
+            {item.price.toFixed(2)}
+          </div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShopItemClass;
